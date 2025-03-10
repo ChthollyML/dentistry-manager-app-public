837a5546d7c46e073e5a37810469cea81db16cea
ChthollyML <2529579870@qq.com>
2025-03-10 21:41:23 +0800

    1

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,9 @@
-import { Row, Col, Card, Form, Input, Button, message } from 'antd';
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { context } from '../components/AppProvider';
-import { loginAPI } from '../services/auth';
-import { defaultImg, setToken } from '../utils/tools';
+import { Row, Col, Card, Form, Input, Button, message } from "antd";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { context } from "../components/AppProvider";
+import { loginAPI } from "../services/auth";
+import { defaultImg, setToken } from "../utils/tools";
 
 function Login() {
   const navigate = useNavigate();
@@ -23,13 +23,13 @@ function Login() {
         <img
           src={defaultImg}
           style={{
-            display: 'block',
-            margin: '20px auto',
-            borderRadius: '16px',
-            width: '200px',
+            display: "block",
+            margin: "20px auto",
+            borderRadius: "16px",
+            width: "200px",
           }}
         />
-        <Card title='好大夫平台管理系统'>
+        <Card title="好大夫平台管理系统">
           <Form
             labelCol={{
               md: {
@@ -37,15 +37,18 @@ function Login() {
               },
             }}
             onFinish={async (v) => {
-              // console.log(v);
               const res = await loginAPI(v);
-              console.log(res);
+              // const res = {
+              //   success: true,
+              //   data: { token: '1234567890' },
+              //   errorMessage: '登录失败',
+              // }
               if (res.success) {
-                message.success('登录成功');
+                message.success("登录成功");
                 setToken(res.data);
                 // 此处可以使用服务器返回的用户角色数据
-                resetMenus('kf'); // 重置路由菜单
-                navigate('/admin/dashboard');
+                resetMenus("kf"); // 重置路由菜单
+                navigate("/admin/dashboard");
               } else {
                 message.error(res.errorMessage);
               }
@@ -54,37 +57,37 @@ function Login() {
             }}
           >
             <Form.Item
-              label='用户名'
-              name='userName'
+              label="用户名"
+              name="userName"
               rules={[
                 {
                   required: true,
-                  message: '请输入用户名',
+                  message: "请输入用户名",
                 },
               ]}
             >
-              <Input placeholder='请输入用户名' />
+              <Input placeholder="请输入用户名" />
             </Form.Item>
             <Form.Item
-              label='密码'
-              name='password'
+              label="密码"
+              name="password"
               rules={[
                 {
                   required: true,
-                  message: '请输入密码',
+                  message: "请输入密码",
                 },
               ]}
             >
-              <Input.Password placeholder='请输入密码' />
+              <Input.Password placeholder="请输入密码" />
             </Form.Item>
             <Form.Item>
               <Button
-                htmlType='submit'
-                type='primary'
+                htmlType="submit"
+                type="primary"
                 style={{
-                  display: 'block',
-                  margin: '8px auto',
-                  width: '20vw',
+                  display: "block",
+                  margin: "8px auto",
+                  width: "20vw",
                 }}
               >
                 登录
